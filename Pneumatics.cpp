278964b60040e213cc985c10a21bb00f942beaf9
Ahmad BAmba <ahmad.eb.bamba@gmail.com>
2014-01-18 08:58:17 -0500

    Fix Foomatics pls

--- Pneumatics.cpp
+++ Pneumatics.cpp
@@ -3,12 +3,15 @@
 #include <DigitalInput.h>
 #include <Relay.h>
 #include "Pneumatics.h"
+#include <Timer.h>
 /*#include <EasterEgg.h>*/
 
-void pneumatics::definePnuematics()
+void pneumatics::definePnuematicsObjects()
 {
     switchObject = new DigitalInput(1, 1);
     compressor = new Relay(1, 1, Relay::kForwardOnly);
+    solenoidTimer = new Timer();
+    testSolenoid = new DoubleSolenoid();
 }
 
 void pneumatics::checkPressure()
@@ -23,4 +26,24 @@ void pneumatics::checkPressure()
     {
         compressor->Set(Relay::kOff);
     }
+}
+
+//Fight Me!
+
+void pnuematics::updateSolenoid(){
+    //This function checks if the solenoid has expired
+    for(int i = 0; i < time.size(); i++){
+        if(timerObject[i]->Get() >= time[i]){
+            solenoid[i]->Set(DoubleSolenoid::kOff);
+            //next, remove from vector
+        }
+    }
+}
+
+void pnuematics::setVectorValues(double timerValues, DoubleSolenoid* startSolenoid, DoubleSolenoid::Value){
+    solenoidTimer = new Timer();
+    testSolenoid = new DoubleSolenoid();
+    timerObject.push_back(timerValues);
+    solenoid.push_back(startSolenoid);
+    testSolenoid->Set(DoubleSolenoid::Value = kOff);
 }
--- Pneumatics.h
+++ Pneumatics.h
@@ -5,14 +5,23 @@
 #include "DriveTrain.h"
 #include <DigitalInput.h>
 #include <Relay.h>
+#include <Timer.h>
+#include <DoubleSolenoid.h>
 
 class pneumatics
 {
     public:
-        void definePnuematics();
+        void definePnuematicsObjects();
         void checkPressure();
+        void setVectorValues(double timerValues, DoubleSolenoid* startSolenoid, DoubleSolenoid::Value);
+        void updateSolenoid();
     private:
         DigitalInput* switchObject;
         Relay* compressor;
+        vector <double> time;
+        vector <Timer*> timerObject
+        vector <DoubleSolenoid*> solenoid;
+        DoubleSolenoid* testSolenoid;
+
 };
 #endif // PNEUMATICS_H
